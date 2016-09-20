f40b6d28ab0fb5c383ceb7ea4ee0773c1b0ddf2f
prangel@gmaill.com <l0ngl1v3wsd>
2016-09-20 06:49:30 -0700

    initial import

---
+++ index.js
@@ -0,0 +1,70 @@
+#! /usr/local/bin/node
+
+var linebyline = require("linebyline"),
+    EventEmitter = require('events').EventEmitter,
+    util = require('util'),
+    lookup = require("./lookup"),
+    assign = require("lodash/Object/assign");
+
+var makeLineParser = function(line) {
+   return {
+      getRecordID : function() {
+         return line.substr(0,3).trim();
+      },
+      getData : function(position, length, process) {
+         var d = line.split("").splice(position-1, length).join("").trim();
+         return (process) ? process(d) : d;
+      }
+   }
+};
+
+
+var fnm_rld = module.exports = function(file, opts) {
+   if (!(this instanceof fnm_rld)) return new fnm_rld(file, opts);
+
+    EventEmitter.call(this);
+    opts = opts || {};
+    if("map" in opts) {
+        if("override" in opts && opts.override === true) {
+            lookup = opts.map;
+        } else {
+            lookup = assign(lookup, opts.map);
+        }
+    }
+
+   var parser,
+       map,
+       item,
+       data = {},
+       self = this;
+
+   var addSection = function(section) {
+      data[section] = [];
+      return data[section];
+   };
+   var fnm_file = linebyline(file);
+   fnm_file.on('line', function(line) {
+      parser = makeLineParser(line);
+      if(parser.getRecordID() in lookup) {
+         map = lookup[parser.getRecordID()];
+         var section = addSection(parser.getRecordID());
+         for(var i=0; i < map.length; i++) {
+            item = map[i];
+            item.data = parser.getData(item.position, item.length, item.process);
+            section.push(
+                {
+                   label : item.label,
+                   data : item.data
+                }
+            );
+         }
+      }
+   }).on('error', function(err) {
+      self.emit('error', err);
+   })
+    .on('close', function() {
+      self.emit('done', data);
+   });
+};
+util.inherits(fnm_rld, EventEmitter);
+
---
+++ lookup.js
@@ -0,0 +1,409 @@
+module.exports = {
+    "01A" : [
+        {
+            "id" : "010",
+            "position" : 1,
+            "length" : 3,
+            "label" : "Mortgage Type and Terms",
+        },
+        {
+            "id" : "020",
+            "position" : 4,
+            "length" : 2,
+            "label" : "Mortgage Applied For (Other)",
+            "process": function(d) {
+                var s = {
+                    "01" : "Conventional",
+                    "02" : "VA",
+                    "03" : "FHA",
+                    "04" : "USDA / Rural Housing Service",
+                    "07" : "Other"
+                };
+                if(d in s) {
+                    return s[d];
+                }
+            }
+        },
+        {
+            "position" : 131,
+            "length" : 15,
+            "label" : "Loan Amount",
+            "process" : function(a) {
+                return parseFloat(a);
+            }
+        },
+        {
+            "position" : 146,
+            "length" : 7,
+            "label" : "Interest Rate",
+            "process" : function(a) {
+                return parseFloat(a);
+            }
+        },
+        {
+            "position" : 153,
+            "length" : 3,
+            "label" : "No. of Months",
+            "process" : function(a) {
+                return parseInt(a);
+            }
+        },
+        {
+            "position" : 156,
+            "length" : 2,
+            "label" : "Amortization Type",
+            "process" : function(a) {
+                var l = {
+                    "01" : "Adjustable Rate",
+                    "04" : "GEM",
+                    "05" : "Fixed Rate",
+                    "06" : "GPM",
+                    "13" : "Other"
+                };
+                if(a in l) {
+                  return l[a];
+                }
+                return "";
+            }
+        },
+        {
+            "position" : 158,
+            "length" : 80,
+            "label" : "Amortization Type Other Explanation"
+        }
+
+    ],
+    "02A" : [
+        {
+            "position" : 1,
+            "length" : 3,
+            "label" : "Information"
+        },
+        {
+            "position" : 4,
+            "length" : 50,
+            "label" : "Street Address"
+        },
+        {
+            "position" : 54,
+            "length" : 35,
+            "label" : "City"
+        },
+        {
+            "position" : 89,
+            "length" : 2,
+            "label" : "State"
+        },
+        {
+            "position" : 91,
+            "length" : 5,
+            "label" : "Zip Code"
+        }
+
+    ],
+    "03A" : [
+        {
+            "position" : 1,
+            "length" : 3,
+            "label" : "Applicant(s) Data"
+        },
+        {
+            "position" : 4,
+            "length" : 2,
+            "label" : "Applicant or Co-Applicant",
+            "process" : function(d) {
+                return (d && d == "BW")? "Applicant" : "Co-Applicant"
+            }
+        },
+        {
+            "position" : 6,
+            "length" : 9,
+            "label" : "SSN"
+        },
+        {
+            "position" : 15,
+            "length" : 35,
+            "label" : "First Name"
+        },
+        {
+            "position" : 50,
+            "length" : 35,
+            "label" : "Middle Name"
+        },
+        {
+            "position" : 85,
+            "length" : 35,
+            "label" : "Last Name"
+        },
+        {
+            "position" : 120,
+            "length" : 4,
+            "label" : "Generation"
+        },
+        {
+            "position" : 124,
+            "length" : 10,
+            "label" : "Home Phone"
+        },
+        {
+            "position" : 134,
+            "length" : 3,
+            "label" : "Age"
+        },
+        {
+            "position" : 137,
+            "length" : 2,
+            "label" : "Years School"
+        },
+        {
+            "position" : 139,
+            "length" : 1,
+            "label" : "Martial Status",
+            "process" : function(d) {
+                var s = {
+                    "M" : "Married",
+                    "S" : "Separated",
+                    "U" : "Unmarried"
+                };
+                if(d in s) {
+                    return s[d];
+                }
+            }
+        }
+    ],
+    "03C" : [
+        {
+            "position" : 1,
+            "length" : 3,
+            "label" : "Applicant(s) Address"
+        },
+        {
+            "position" : 4,
+            "length" : 9,
+            "label" : "SSN"
+        },
+        {
+            "position" : 13,
+            "length" : 2,
+            "label" : "Residency Type",
+            "process" : function(d) {
+                var s = {
+                    "F4" : "Former Residence",
+                    "ZG" : "Present Residence",
+                    "BH" : "Mailing Address"
+                };
+                if(d in s) {
+                    return s[d];
+                }
+            }
+        },
+        {
+            "position" : 15,
+            "length" : 50,
+            "label" : "Street Address"
+        },
+        {
+            "position" : 65,
+            "length" : 35,
+            "label" : "City"
+        },
+        {
+            "position" : 100,
+            "length" : 2,
+            "label" : "State"
+        },
+        {
+            "position" : 102,
+            "length" : 5,
+            "label" : "Zip Code"
+        },
+        {
+            "position" : 107,
+            "length" : 4,
+            "label" : "Zip Code Plus Four"
+        },
+        {
+            "position" : 111,
+            "length" : 1,
+            "label" : "Basis Type",
+            "process": function(d) {
+                var s = {
+                    "X" : "Living Rent Free",
+                    "R" : "Rent",
+                    "O" : "Own"
+                };
+                if(d in s) {
+                    return s[d];
+                }
+            }
+        },
+        {
+            "position" : 112,
+            "length" : 2,
+            "label" : "Duration Years",
+            "process": function(d) {
+                return (d)? parseInt(d) : "";
+            }
+        },
+        {
+            "position" : 114,
+            "length" : 2,
+            "label" : "Duration Months",
+            "process": function(d) {
+                if(d) {
+                    d = parseInt(d);
+                    if(d > 11 && d < 0) {
+                        d = "";
+                    }
+                }
+                return d;
+            }
+        },
+        {
+            "position" : 116,
+            "length" : 50,
+            "label" : "Country"
+        }
+    ],
+    "04A" : [
+        {
+            "position" : 1,
+            "length" : 3,
+            "label" : "Borrower Employer"
+        },
+        {
+            "position" : 4,
+            "length" : 9,
+            "label" : "SSN"
+        },
+        {
+            "position" : 13,
+            "length" : 35,
+            "label" : "Employer Name"
+        },
+        {
+            "position" : 48,
+            "length" : 35,
+            "label" : "Employer Street Address"
+        },
+        {
+            "position" : 83,
+            "length" : 35,
+            "label" : "Employer City"
+        },
+        {
+            "position" : 118,
+            "length" : 2,
+            "label" : "Employer State"
+        },
+        {
+            "position" : 120,
+            "length" : 5,
+            "label" : "Employer Zip Code"
+        },
+        {
+            "position" : 125,
+            "length" : 4,
+            "label" : "Employer Zip Code Plus Four"
+        },
+        {
+            "position" : 129,
+            "length" : 1,
+            "label" : "Self Employed"
+        },
+        {
+            "position" : 130,
+            "length" : 2,
+            "label" : "Years On Job"
+        },
+        {
+            "position" : 132,
+            "length" : 2,
+            "label" : "Months On Job"
+        },
+        {
+            "position" : 134,
+            "length" : 2,
+            "label" : "Number of Years in this line of work"
+        },
+        {
+            "position" : 136,
+            "length" : 25,
+            "label" : "Employment Position Description"
+        },
+        {
+            "position" : 161,
+            "length" : 10,
+            "label" : "Employer Phone Number"
+        }
+    ],
+    "10B" : [
+        {
+            "position" : 1,
+            "length" : 3,
+            "label" : "Loan Originator Information"
+        },
+        {
+            "position" : 4,
+            "length" : 1,
+            "label" : "This application was taken by",
+            "process" : function(d) {
+                var s = {
+                    "F" : "Face-to-Face",
+                    "M" : "Mail or Fax",
+                    "T" : "Telephone",
+                    "I" : "Internet or E-Mail"
+                };
+                if(d in s) {
+                    return s[d];
+                }
+            }
+        },
+        {
+            "position" : 5,
+            "length" : 60,
+            "label" : "Name"
+
+        },
+        {
+            "position" : 65,
+            "length" : 8,
+            "label" : "The date of loan origin"
+        },
+        {
+            "position" : 73,
+            "length" : 10,
+            "label" : "Phone number"
+        },
+        {
+            "position" : 83,
+            "length" : 35,
+            "label" : "Name"
+        },
+        {
+            "position" : 118,
+            "length" : 35,
+            "label" : "Street Address"
+        },
+        {
+            "position" : 153,
+            "length" : 35,
+            "label" : "Street Address 2"
+        },
+        {
+            "position" : 223,
+            "length" : 2,
+            "label" : "State Code"
+        },
+        {
+            "position" : 225,
+            "length" : 5,
+            "label" : "Zip Code"
+        },
+        {
+            "position" : 230,
+            "length" : 4,
+            "label" : "Zip Code Plus 4"
+        }
+    ]
+};
